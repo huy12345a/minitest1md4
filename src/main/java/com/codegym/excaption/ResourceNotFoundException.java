133cf661f89f770999b0164576aa1ff42ce5c038
huy12345a <Quanghuy.hmtb@gmail.com>
2024-09-30 08:18:43 +0700

    2

--- src/main/java/com/codegym/excaption/ResourceNotFoundException.java
+++ src/main/java/com/codegym/excaption/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.codegym.excaption;
-
-public class ResourceNotFoundException {
-    public ResourceNotFoundException(String tourNotFound, Object message) {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
+
