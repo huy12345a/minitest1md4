0695cbbe7a999b9fc980c7000b7171284cf17440
huy12345a <Quanghuy.hmtb@gmail.com>
2024-10-01 09:06:19 +0700

    cap nhat

--- src/main/java/com/codegym/controller/TourController.java
+++ src/main/java/com/codegym/controller/TourController.java
@@ -1,22 +1,74 @@
 package com.codegym.controller;
 
 import com.codegym.model.Tour;
-import com.codegym.service.TourService;
+import com.codegym.service.ITourService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/tours")
+@Controller
+@RequestMapping("/tours")
 public class TourController {
     @Autowired
-    private TourService tourService;
-    public ModelAndView showList()
+    private ITourService tourService;
+
+    @GetMapping
+    public String tours(Model model) {
+        Iterable<Tour> tours = tourService.getTours();
+        model.addAttribute("tours", tours);
+        return "/tour/list";
     }
 
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("tour", new Tour());
+        return "/tour/create";
+    }
 
+    @PostMapping("/save")
+    public String save(Tour tour) {
+        tourService.save(tour);
+        return "redirect:/tours";
+    }
 
+    @GetMapping("/{id}/edit")
+    public String update(@PathVariable Long id, Model model) {
+        Optional<Tour> tour = tourService.getTour(id);
+        model.addAttribute("tour", tour.get());
+        return "/tour/update";
+    }
+
+    @PostMapping("/update")
+    public String update(Tour tour) {
+        tourService.save(tour);
+        return "redirect:/tours";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable Long id, Model model) {
+        Optional<Tour> tour = tourService.getTour(id);
+        model.addAttribute("tour", tour.get());
+        return "/tour/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Tour tour) {
+        tourService.deleteTour(tour.getId());
+//        redirect.addFlashAttribute("success", "Removed customer successfully!");
+        return "redirect:/tours";
+    }
+
+    @GetMapping("/{id}/view")
+    public String view(@PathVariable Long id, Model model) {
+      Optional<Tour> tour = tourService.getTour(id);
+        model.addAttribute("tour",tour.get());
+        return "/tour/view";
+    }
+
+}
--- src/main/java/com/codegym/repository/TourRepository.java
+++ src/main/java/com/codegym/repository/TourRepository.java
@@ -5,7 +5,9 @@ import com.codegym.model.Tour;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TourRepository extends JpaRepository<Tour, Long> {
     Iterable<Tour> findAllByProvince(Province province);
 
